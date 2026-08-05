18ef890ca8b782d9f21a0163805b00ea6c7d1dda
Merkle Bonsai <merklebonsai@pm.me>
2026-08-05 19:44:03 +0700

    feat(pew): InProcess/Surface launch definitions join the typed triad

--- packages/plexus-expectation/src/shared/models/SurfaceLaunchDefinition.ts
+++ packages/plexus-expectation/src/shared/models/SurfaceLaunchDefinition.ts
@@ -1,7 +1,8 @@
 import { syncing } from "@here.build/plexus";
 
+import type { AnyExpectation, Expectation } from "./Expectation.js";
 import { LaunchDefinition } from "./LaunchDefinition.js";
 
 /** Human-fulfilled work: inert actor; settlement is settleSurface. No core timeout. */
 @syncing("@here.build/plexus-expectation:SurfaceLaunchDefinition")
-export class SurfaceLaunchDefinition extends LaunchDefinition {}
+export class SurfaceLaunchDefinition<E extends AnyExpectation = Expectation> extends LaunchDefinition<E> {}
